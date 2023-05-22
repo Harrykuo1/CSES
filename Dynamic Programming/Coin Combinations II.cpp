88fb2955f999e1a3ce9be88c341897b67b9feb11
Harrykuo1 <stu970157@gmail.com>
2023-05-23 05:48:37 +0800

    Complete Coin Combinations II

---
+++ Dynamic Programming/Coin Combinations II.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+#define mod 1000000007
+
+int dp[10000005];
+
+int32_t main(){
+
+    int n, x, num;
+    scanf("%lld %lld", &n, &x);
+    vector<int> vec;
+    for(int i=0;i<n;i++){
+        scanf("%lld", &num);
+        vec.push_back(num);
+    }
+    dp[0] = 1;
+
+    for(int j=0;j<n;j++){
+        for(int i=0;i<=x;i++){
+            if(i<vec[j]){
+                continue;
+            }
+            dp[i] += dp[i-vec[j]];
+            dp[i] %= mod;
+        }
+    }
+    printf("%lld\n", dp[x]);
+}
